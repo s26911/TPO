115c485f5fb122ace5fb6b0992c6dd05ef7f28f6
Jakub Gołębiewski <s26911@pjwstk.edu.pl>
2024-05-10 15:49:35 +0200

    Added readLine() using buffer

--- TPO_04/src/Server.java
+++ TPO_04/src/Server.java
@@ -1,28 +1,29 @@
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
-public class Server{
+public class Server {
     // TO IMPLEMENT
     // Maintain topic collection
     // Listen for commands from admin
     // Subscribe the clients and maintain client database
-    ArrayList<String> topics = new ArrayList<>();
-    HashMap<String, SelectionKey> clients;
     boolean isRunning = true;
+    HashMap<String, ArrayList<SocketChannel>> topicsClients = new HashMap<>();      // key = topic name
+    ByteBuffer buffer = ByteBuffer.allocate(1024);                          // value = list of subscribed clients channels
+
     ServerSocketChannel serverSocketChannel;
     Selector selector;
     String address;
     int port;
-    HashMap<String, ArrayList<SocketChannel>> topicsClients = new HashMap<>();      // key = topic name
-                                                                                    // value = list of subscribed clients channels
+
     public Server(String address, int port) {
         this.address = address;
         this.port = port;
@@ -43,7 +44,7 @@ public class Server{
     }
 
     private void handleConnections() throws IOException {
-        while(isRunning){
+        while (isRunning) {
             selector.select();
             var keys = selector.selectedKeys();
             var iter = keys.iterator();
@@ -55,16 +56,36 @@ public class Server{
                     SocketChannel incoming = serverSocketChannel.accept();
                     incoming.configureBlocking(false);
                     incoming.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
-                }
-                else if (key.isReadable()) {
+                } else if (key.isReadable()) {
                     SocketChannel incoming = (SocketChannel) key.channel();
                     handleRequest(incoming);
-                }
-                else if (key.isWritable()) {
+                } else if (key.isWritable()) {
                     // TODO?
                 }
             }
         }
+    }
+
+    private String readLine (SocketChannel incoming) throws IOException {
+        buffer.clear();
+
+        StringBuilder line = new StringBuilder();
+        outer:
+        while (true) {
+            if (incoming.read(buffer) > 0) {
+                buffer.flip();
+                CharBuffer cbuf = StandardCharsets.UTF_8.decode(buffer);
+
+                while (cbuf.hasRemaining()) {
+                    char c = cbuf.get();
+                    if (c == '\n')
+                        break outer;
+                    line.append(c);
+                }
+            }
+        }
+
+        return line.toString();
     }
 
     private void handleRequest(SocketChannel incoming) throws IOException {
